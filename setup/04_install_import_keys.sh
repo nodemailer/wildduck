cf81c82da1bdccbcd6e61f337b41879362f89891
Andris Reinman <andris@kreata.ee>
2018-08-06 09:27:36 +0300

    updated install script

--- setup/04_install_import_keys.sh
+++ setup/04_install_import_keys.sh
@@ -21,30 +21,30 @@ chown -R deploy:deploy /home/deploy
 
 export DEBIAN_FRONTEND=noninteractive
 
-# mongo keys
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58712A2291FA4AD5
-gpg --armor --export 58712A2291FA4AD5 | apt-key add -
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+# nodejs
+wget -qO- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+echo "deb https://deb.nodesource.com/$NODEREPO $CODENAME main" > /etc/apt/sources.list.d/nodesource.list
+echo "deb-src https://deb.nodesource.com/$NODEREPO $CODENAME main" >> /etc/apt/sources.list.d/nodesource.list
 
+# mongo keys
+wget -qO- https://www.mongodb.org/static/pgp/server-${MONGODB}.asc | sudo apt-key add
+# hardcode xenial as at this time there are no non-dev packages for bionic (http://repo.mongodb.org/apt/ubuntu/dists/)
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/$MONGODB multiverse" > /etc/apt/sources.list.d/mongodb-org.list
 
 # tor keys
 # Setup tor to be able to send emails to .onion network.
 # Receiving from onion is not automatically set up, you would have to
 # create a hidden servcie first and then bridge port 25
-echo 'deb http://deb.torproject.org/torproject.org xenial main
-deb-src http://deb.torproject.org/torproject.org xenial main' > /etc/apt/sources.list.d/tor.list
+echo "deb http://deb.torproject.org/torproject.org $CODENAME main
+deb-src http://deb.torproject.org/torproject.org $CODENAME main" > /etc/apt/sources.list.d/tor.list
 gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
 # rspamd
-CODENAME=`lsb_release -c -s`
 wget -O- https://rspamd.com/apt-stable/gpg.key | apt-key add -
 echo "deb http://rspamd.com/apt-stable/ $CODENAME main" > /etc/apt/sources.list.d/rspamd.list
 echo "deb-src http://rspamd.com/apt-stable/ $CODENAME main" >> /etc/apt/sources.list.d/rspamd.list
 apt-get update
 
-apt-get -q -y install redis-server clamav clamav-daemon
-apt-get -q -y --no-install-recommends install rspamd
-
-apt-get clean
+# redis
+apt-add-repository -y ppa:chris-lea/redis-server
--- setup/05_install_packages.sh
+++ setup/05_install_packages.sh
@@ -6,20 +6,13 @@ echo -e "\n-- Executing ${ORANGE}${OURNAME}${NC} subscript --"
 
 # install nginx
 apt-get update
-apt-get -q -y install pwgen git ufw build-essential libssl-dev dnsutils python software-properties-common nginx lsb-release wget
-
-# install node 8
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get -q -y install pwgen git ufw build-essential libssl-dev dnsutils python software-properties-common nginx wget
 
 # install tor
 apt-get update
 
-apt-get -q -y install mongodb-org nodejs tor deb.torproject.org-keyring
-
-# redis
-apt-add-repository -y ppa:chris-lea/redis-server
+apt-get -q -y install mongodb-org nodejs tor deb.torproject.org-keyring redis-server clamav clamav-daemon
 
 # rspamd
-apt-get -q -y install redis-server clamav clamav-daemon
 apt-get -q -y --no-install-recommends install rspamd
 apt-get clean
