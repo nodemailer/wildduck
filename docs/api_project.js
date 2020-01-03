64ccb33ee5723d1ff9559ac83a9d685aead185cc
Andris Reinman <andris@kreata.ee>
2020-01-03 13:30:42 +0200

    bumped deps

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,1 +1,16 @@
-define({  "name": "wildduck",  "version": "1.0.0",  "description": "WildDuck API docs",  "title": "WildDuck API",  "url": "https://api.wildduck.email",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-11-21T22:27:44.247Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
+define({
+  "name": "wildduck",
+  "version": "1.0.0",
+  "description": "WildDuck API docs",
+  "title": "WildDuck API",
+  "url": "https://api.wildduck.email",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-01-03T11:29:51.496Z",
+    "url": "http://apidocjs.com",
+    "version": "0.19.1"
+  }
+});
