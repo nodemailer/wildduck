a38d944fde6e3e6b7219404c81a0ca42a95da0e1
Andris Reinman <andris@kreata.ee>
2020-09-18 10:22:50 +0300

    v1.29.4

--- imap-core/lib/indexer/indexer.js
+++ imap-core/lib/indexer/indexer.js
@@ -628,7 +628,7 @@ class Indexer {
                 }
                 mimeTree.attachmentMap[node.attachmentId] = id;
 
-                let attachmentInfo = mimeTree.attachments.find(a => a.id === node.attachmentId);
+                let attachmentInfo = mimeTree.attachments && mimeTree.attachments.find(a => a.id === node.attachmentId);
                 if (attachmentInfo && node.body) {
                     attachmentInfo.size = node.body.length;
                 }
