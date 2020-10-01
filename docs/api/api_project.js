61eb05717c2af6b3068a7617fbc5b4d9897c184a
Andris Reinman <andris@kreata.ee>
2020-10-01 11:01:20 +0300

    v1.30.1

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-09-28T10:51:58.252Z",
+    "time": "2020-09-28T10:56:44.910Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
