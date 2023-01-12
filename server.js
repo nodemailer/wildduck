a416228eeb9e0adaf015fd70004e7669e63635d7
Andris Reinman <andris@reinman.eu>
2023-01-12 11:43:01 +0200

    log es setup status

--- server.js
+++ server.js
@@ -67,6 +67,9 @@ if (!processCount || processCount <= 1) {
     // single process mode, do not fork anything
 
     initElasticSearch()
+        .then(() => {
+            log.info('App', `ElasticSearch setup checked`);
+        })
         .catch(err => {
             log.error('App', `ElasticSearch setup failed: ${err.message}${err.meta?.statusCode ? ` (${err.meta?.statusCode})` : ''}`);
         })
@@ -95,6 +98,9 @@ if (!processCount || processCount <= 1) {
 
         // Fork workers.
         initElasticSearch()
+            .then(() => {
+                log.info('App', `ElasticSearch setup checked`);
+            })
             .catch(err => {
                 log.error('App', `ElasticSearch setup failed: ${err.message}${err.meta?.statusCode ? ` (${err.meta?.statusCode})` : ''}`);
             })
