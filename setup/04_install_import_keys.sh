c51020b93bcaf81ccac0059832ba282e16680fd2
Andris Reinman <andris@reinman.eu>
2023-08-24 10:49:00 +0300

    updated setup scripts

--- setup/04_install_import_keys.sh
+++ setup/04_install_import_keys.sh
@@ -24,25 +24,31 @@ keyring="/usr/share/keyrings"
 
 # nodejs
 node_key_url="https://deb.nodesource.com/gpgkey/nodesource.gpg.key"
-curl -s $node_key_url | gpg --dearmor | tee ${keyring}/nodesource.gpg >/dev/null
+local_node_key="${keyring}/nodesource.gpg"
+curl -fsSL $node_key_url | gpg --dearmor | tee $local_node_key >/dev/null
 
-echo "deb https://deb.nodesource.com/$NODEREPO $CODENAME main" > /etc/apt/sources.list.d/nodesource.list
-echo "deb-src https://deb.nodesource.com/$NODEREPO $CODENAME main" >> /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=${local_node_key}] https://deb.nodesource.com/$NODEREPO $CODENAME main" > /etc/apt/sources.list.d/nodesource.list
+echo "deb-src [signed-by=${local_node_key}] https://deb.nodesource.com/$NODEREPO $CODENAME main" >> /etc/apt/sources.list.d/nodesource.list
 
 # mongodb
 mongo_key_url="https://pgp.mongodb.com/server-${MONGODB}.asc"
-curl -s $mongo_key_url | gpg --dearmor | tee ${keyring}/mongodb-server-${MONGODB}.gpg >/dev/null
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${CODENAME}/mongodb-org/${MONGODB} multiverse" | tee /etc/apt/sources.list.d/mongodb-org-${MONGODB}.list
+local_mongo_key="${keyring}/mongodb-server-${MONGODB}.gpg"
+curl -fsSL $mongo_key_url | gpg --dearmor | tee ${local_mongo_key} >/dev/null
+echo "deb [ arch=amd64,arm64 signed-by=${local_mongo_key} ] https://repo.mongodb.org/apt/ubuntu ${CODENAME}/mongodb-org/${MONGODB} multiverse" > /etc/apt/sources.list.d/mongodb-org-${MONGODB}.list
 
 # rspamd
 rspamd_key_url="https://rspamd.com/apt-stable/gpg.key"
-curl -s $rspamd_key_url | gpg --dearmor | tee ${keyring}/rspamd.gpg >/dev/null
+local_rspamd_key="${keyring}/rspamd.gpg"
+curl -fsSL $rspamd_key_url | gpg --dearmor | tee ${local_rspamd_key} >/dev/null
 
-echo "deb http://rspamd.com/apt-stable/ $CODENAME main" > /etc/apt/sources.list.d/rspamd.list
-echo "deb-src http://rspamd.com/apt-stable/ $CODENAME main" >> /etc/apt/sources.list.d/rspamd.list
+echo "deb [signed-by=${local_rspamd_key}] http://rspamd.com/apt-stable/ $CODENAME main" > /etc/apt/sources.list.d/rspamd.list
+echo "deb-src [signed-by=${local_rspamd_key}] http://rspamd.com/apt-stable/ $CODENAME main" >> /etc/apt/sources.list.d/rspamd.list
 
 # redis
-curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o ${keyring}/redis-archive-keyring.gpg
-echo "deb [signed-by=${keyring}/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+redis_key_url="https://packages.redis.io/gpg"
+local_redis_key="${keyring}/redis-archive-keyring.gpg"
+curl -fsSL $redis_key_url | gpg --dearmor | tee ${local_redis_key} >/dev/null
+
+echo "deb [signed-by=${local_redis_key}] https://packages.redis.io/deb $CODENAME main" > tee /etc/apt/sources.list.d/redis.list
 
 apt-get update
