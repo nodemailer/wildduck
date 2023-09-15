6f27e0cff0e0dd744fcf8200437b2500702afab6
Nikolai Ovtsinnikov <nikolai@zone.ee>
2023-09-15 10:46:10 +0300

    added multiple tests for all storage api endpoints. Both positive and negative tests

--- test/api/storage-test.js
+++ test/api/storage-test.js
@@ -58,5 +58,142 @@ describe('Storage tests', function () {
             })
             .expect(200);
         expect(response.body.success).to.be.true;
+        expect(response.body.id).to.be.not.empty;
+    });
+
+    it('should POST /users/{user}/storage expect success / filename undefined', async () => {
+        const response = await server
+            .post(`/users/${user}/storage`)
+            .send({
+                contentType: 'image/gif',
+                encoding: 'base64',
+                content:
+                    'R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7'
+            })
+            .expect(200);
+        expect(response.body.success).to.be.true;
+        expect(response.body.id).to.be.not.empty;
+    });
+
+    it('should POST /users/{user}/storage expect success / filename undefined, contentType undefined', async () => {
+        const response = await server
+            .post(`/users/${user}/storage`)
+            .send({
+                encoding: 'base64',
+                content:
+                    'R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7'
+            })
+            .expect(200);
+        expect(response.body.success).to.be.true;
+        expect(response.body.id).to.be.not.empty;
+    });
+
+    it('should POST /users/{user}/storage expect failure / content missing', async () => {
+        const response = await server
+            .post(`/users/${user}/storage`)
+            .send({
+                filename: 'image.gif',
+                contentType: 'image/gif',
+                encoding: 'base64'
+            })
+            .expect(400);
+        expect(response.body.code).to.be.equal('InputValidationError');
+    });
+
+    it('should POST /users/{user}/storage expect failure / incorrect user id', async () => {
+        const response = await server
+            .post(`/users/123/storage`)
+            .send({
+                filename: 'image.gif',
+                contentType: 'image/gif',
+                encoding: 'base64',
+                content:
+                    'R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7'
+            })
+            .expect(400);
+        expect(response.body.code).to.be.equal('InputValidationError');
+    });
+
+    it('should POST /users/{user}/storage expect failure / user not found', async () => {
+        const response = await server
+            .post(`/users/${Array.from(user).reverse().join('')}/storage`)
+            .send({
+                filename: 'image.gif',
+                contentType: 'image/gif',
+                encoding: 'base64',
+                content:
+                    'R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7'
+            })
+            .expect(404);
+        expect(response.body.code).to.be.equal('UserNotFound');
+        expect(response.body.error).to.be.equal('This user does not exist');
+    });
+
+    it('should GET /users/{user}/storage expect success', async () => {
+        const response = await server.get(`/users/${user}/storage`).send({}).expect(200);
+        expect(response.body.success).to.be.true;
+    });
+
+    it('should GET /users/{user}/storage expect success / different limit', async () => {
+        const response = await server.get(`/users/${user}/storage?limit=5`).send({}).expect(200);
+        expect(response.body.success).to.be.true;
+    });
+
+    it('should GET /users/{user}/storage expect failure / incorrect limit', async () => {
+        const response = await server.get(`/users/${user}/storage?limit=1000`).send({}).expect(400);
+        expect(response.body.code).to.be.equal('InputValidationError');
+    });
+
+    it('should GET /users/{user}/storage expect failure / user not found', async () => {
+        const response = await server
+            .get(`/users/${Array.from(user).reverse().join('')}/storage`)
+            .send({})
+            .expect(404);
+        expect(response.body.code).to.be.equal('UserNotFound');
+        expect(response.body.error).to.be.equal('This user does not exist');
+    });
+
+    it('should DELETE /users/{user}/storage/{file} expect success', async () => {
+        const userFiles = await server.get(`/users/${user}/storage`).send({}).expect(200);
+
+        const response = await server.del(`/users/${user}/storage/${userFiles.body.results[0].id}`).send({}).expect(200);
+        expect(response.body.success).to.be.true;
+    });
+
+    it('should DELETE /users/{user}/storage/{file} expect failure / file format incorrect', async () => {
+        const response = await server.del(`/users/${user}/storage/${123}`).send({}).expect(400);
+        expect(response.body.code).to.be.equal('InputValidationError');
+    });
+
+    it('should DELETE /users/{user}/storage/{file} expect failure / file missing, incorrect file id', async () => {
+        const userFiles = await server.get(`/users/${user}/storage`).send({}).expect(200);
+
+        const response = await server
+            .del(`/users/${user}/storage/${Array.from(userFiles.body.results[0].id).reverse().join('')}`)
+            .send({})
+            .expect(404);
+        expect(response.body.code).to.be.equal('FileNotFound');
+    });
+
+    it('should GET /users/{user}/storage/{file} expect success', async () => {
+        const userFiles = await server.get(`/users/${user}/storage`).send({}).expect(200);
+
+        const response = await server.del(`/users/${user}/storage/${userFiles.body.results[0].id}`).send({}).expect(200);
+        expect(response.body.success).to.be.true;
+    });
+
+    it('should GET /users/{user}/storage/{file} expect failure / file format incorrect', async () => {
+        const response = await server.get(`/users/${user}/storage/${123}`).send({}).expect(400);
+        expect(response.body.code).to.be.equal('InputValidationError');
+    });
+
+    it('should GET /users/{user}/storage/{file} expect failure / file missing, incorrect file id', async () => {
+        const userFiles = await server.get(`/users/${user}/storage`).send({}).expect(200);
+
+        const response = await server
+            .get(`/users/${user}/storage/${Array.from(userFiles.body.results[0].id).reverse().join('')}`)
+            .send({})
+            .expect(404);
+        expect(response.body.code).to.be.equal('FileNotFound');
     });
 });
