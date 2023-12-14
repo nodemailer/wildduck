17e9d27d3470e3a27acd3d9b88543eafb8ea50f2
Andris Reinman <andris@reinman.eu>
2023-12-14 11:51:28 +0200

    Bumped deps

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,9 @@ module.exports = {
     upgrade: true,
     reject: [
         // mongodb 5.x driver does not support callbacks, only promises
-        'mongodb'
+        'mongodb',
+
+        // no support for Node 16
+        'undici'
     ]
 };
