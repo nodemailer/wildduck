896a7cb1afe6a112e90aef57ae83b7f97db8eb35
Andris Reinman <andris@reinman.eu>
2024-01-02 12:42:52 +0200

    Updated Node install script

--- setup/04_install_import_keys.sh
+++ setup/04_install_import_keys.sh
@@ -27,8 +27,8 @@ node_key_url="https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key"
 local_node_key="${keyring}/nodesource.gpg"
 curl -fsSL $node_key_url | gpg --dearmor | tee $local_node_key >/dev/null
 
-echo "deb [signed-by=${local_node_key}] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src [signed-by=${local_node_key}] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=${local_node_key}] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" > /etc/apt/sources.list.d/nodesource.list
+echo "deb-src [signed-by=${local_node_key}] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" >> /etc/apt/sources.list.d/nodesource.list
 
 # mongodb
 mongo_key_url="https://pgp.mongodb.com/server-${MONGODB}.asc"
