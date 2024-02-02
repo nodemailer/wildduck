987476bec2fd3a820a65e4439f2ca01df10c2fff
Nikolai Ovtsinnikov <nikolai@zone.ee>
2024-02-02 12:55:37 +0200

    add restifyapigenerate package and use it, fix path

--- api.js
+++ api.js
@@ -648,7 +648,7 @@ module.exports = done => {
                 }
             },
             security: [{ AccessTokenAuth: [] }],
-            docsPath: '/openapidocs.json'
+            docsPath: '/../../openapidocs.json'
         })
     );
 
