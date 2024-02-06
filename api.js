9bf2ec4932006c850b958d58a4947e842c0e3c5d
Nikolai Ovtsinnikov <nikolai@zone.ee>
2024-02-06 10:39:50 +0200

    api.js, filterroutes, remove unnecessary loggelf param

--- api.js
+++ api.js
@@ -549,7 +549,7 @@ module.exports = done => {
     mailboxesRoutes(db, server, mailboxHandler);
     messagesRoutes(db, server, messageHandler, userHandler, storageHandler, settingsHandler);
     storageRoutes(db, server, storageHandler);
-    filtersRoutes(db, server, userHandler, settingsHandler, loggelf);
+    filtersRoutes(db, server, userHandler, settingsHandler);
     domainaccessRoutes(db, server);
     aspsRoutes(db, server, userHandler);
     totpRoutes(db, server, userHandler);
